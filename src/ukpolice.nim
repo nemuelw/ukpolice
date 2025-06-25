d7b925610104ea7c736137aa922a0ea41bb2c197
Nemuel Wainaina <nemuelwainaina@proton.me>
2025-06-25 06:55:36 +0300

    Add function to get detailed info about a specific force

--- src/ukpolice.nim
+++ src/ukpolice.nim
@@ -4,15 +4,32 @@ const BaseUrl = "https://data.police.uk/api/"
 
 type
   CrimeDate* = object
-    date: string
-    stop_and_search: seq[string]
+    date*: string
+    stop_and_search*: seq[string]
 
   Force* = object
-    id: string
-    name: string
+    id*: string
+    name*: string
+
+  EngagementMethod* = object
+    engagement_type*: string
+    title*: string
+    description*: string
+    url*: string
+
+  ForceDetails* = object
+    id*: string
+    name*: string
+    description*: string
+    url*: string
+    telephone*: string
+    engagement_methods*: seq[EngagementMethod]    
 
 proc renameHook(c: var CrimeDate, fieldName: var string) =
   if fieldName == "stop-and-search": fieldName = "stop_and_search"
+
+proc renameHook(e: var EngagementMethod, fieldName: var string) =
+  if fieldName == "type": fieldName = "engagement_type"
 
 let client = newHttpClient()
 client.headers["User-Agent"] = "ukpolice/0.1.0 (Nim)"
@@ -24,3 +41,7 @@ proc get_available_crime_dates*(): seq[CrimeDate] =
 proc get_forces*(): seq[Force] =
   let resp = client.getContent(BaseUrl & "forces")
   resp.fromJson(seq[Force])
+
+proc get_force_details*(force_id: string): ForceDetails =
+  let resp = client.getContent(BaseUrl & "forces" & "/" & force_id)
+  resp.fromJson(ForceDetails)
--- tests/test_ukpolice.nim
+++ tests/test_ukpolice.nim
@@ -8,3 +8,9 @@ test "get_available_crime_dates() returns non-empty list":
 test "get_forces() returns non-empty list":
   let forces = get_forces()
   check forces.len > 0
+
+test "get_force_details(force_id) returns correct force details":
+  let details = get_force_details("leicestershire")
+  check details.id == "leicestershire"
+  check details.name == "Leicestershire Police"
+  check details.telephone == "101"
